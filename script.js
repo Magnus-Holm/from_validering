a67b54edfd00bfe65b91d05d05e1d6b596b98782
Magnus Holm <196121@rts-365.dk>
2018-05-17 13:13:37 +0200

    java update2

--- script.js
+++ script.js
@@ -6,12 +6,162 @@ function Valider() {
     var AntalFejl = 0;
     var FejlBeskedNavn = "";
 
-    if (document.getElementById('navn').value === "") {
+    if (document.getElementById('forNavn').value === "") {
         AntalFejl += 1;
         FejlBeskedNavn = "Dit navn mangler";
     } else {
         var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
         if (!regexpnavn.test(document.getElementById('navn').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('efterNavn').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('aaTal').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('maaned').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('dag').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('sex').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('adresse').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('vejNr').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('postNr').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('by').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('land').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('tlfNr').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('eMail').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('adresse2').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('vejNr2').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
+            AntalFejl += 1;
+            FejlBeskedNavn = "Navnet er ikke gyldig";
+        }
+    }
+    if (document.getElementById('').value === "") {
+        AntalFejl += 1;
+        FejlBeskedNavn = "Dit navn mangler";
+    } else {
+        var regexpnavn = /^[a-zA-ZÆØÅæøå]+[\s]*[a-zA-ZÆØÅæøå]*[\s]*[a-zA-ZÆØÅæøå]*$/;
+        if (!regexpnavn.test(document.getElementById('').value)) {
             AntalFejl += 1;
             FejlBeskedNavn = "Navnet er ikke gyldig";
         }
